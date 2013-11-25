b005b374369df5e3a3f544d21fd8322de1d95e82
tonussi <lptonussi@gmail.com>
2013-11-25 17:05:50 -0200

    Add a empty pytest

---
+++ test/testse.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from pyswip import *
+
+
+class TestExamples(unittest.TestCase):
+
+    # define setup
+    # test prolog comm
+    # test methods
+    # test knowledge
+
+if __name__ == "__main__":
+    unittest.main()
+
